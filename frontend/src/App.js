13017a4eb2265b1e570da2ce76fd60d53042bf6a
Muhanaawahyu <muhanaawahyu00@gmail.com>
2023-02-25 15:00:25 +0700

    list-products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,34 @@
+/* eslint-disable jsx-a11y/alt-text */
+import data from './data'
+
 function App() {
   return (
     <div>
       <header className="bg-slate-900 p-5">
         <a href="/" className="text-slate-100 font-bold">Tukupedia</a>
       </header>
+      <main className="p-4 w-full min-h-screen px-10 py-5 bg-teal-200">
+        <h1>Featured Products</h1>
+        <div className="grid mt-5 grid-flow-row gap-3 justify-center justify-items-center lg:grid-cols-6 md:grid-cols-4 grid-cols-2 bg-slate-400">
+          {data.products.map((product) => (
+            <div className="flex">
+              <div key={product.slug} className="bg-yellow-400 rounded-md max-w-sm overflow-hidden shadow-md" >
+                <a href={`/product/${product.slug}`}>
+                  <img src={product.image} alt={product.name} className="rounded-t-md w-full" />
+                </a>
+                <div className="p-4">
+                  <a href={`/product/${product.slug}`}>
+                    <p className="overvlow-ellipsis overflow-hidden break-keep whitespace-pre-wrap">{product.name}</p>
+                  </a>
+                  <p>{product.price}</p>
+                  <p>{product.rating}</p>
+                  <p>{product.numReviews}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products: [
+        {
+            name: 'Indomilk Steril Can Plain 189 ml',
+            slug: 'indomilk-steril-can-plain-189-ml',
+            category: 'Foods & Drinks',
+            image: '/images/p1.jpg', //679 x 829
+            price: 26000,
+            stock: 12,
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Buy 2 Get 1 pcs - Indomilk Steril Can Plain 189 ml'
+        },
+        {
+            name: 'Indomie Goreng Spesial',
+            slug: 'indomie-goreng-spesial',
+            category: 'Foods & Drinks',
+            image: '/images/p2.jpg',
+            price: 15000,
+            stock: 12,
+            rating: 4.5,
+            numReviews: 10,
+            description: '4+1 pcs Indomie Goreng Spesial'
+        },
+        {
+            name: 'Chitato Wavy Beef BBQ 35 gr',
+            slug: 'chitato-wavy-beef-bbq-35-gr',
+            category: 'Foods & Drinks',
+            image: '/images/p3.jpg',
+            price: 6000,
+            stock: 12,
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Chitato Wavy Beef BBQ 35 gr'
+        },
+        {
+            name: 'Tepung Terigu Segitiga Biru 1 kg',
+            slug: 'tepung-terigu-segitiga-biru-1-kg',
+            category: 'Foods & Drinks',
+            image: '/images/p4.jpg',
+            price: 28000,
+            stock: 12,
+            rating: 4.5,
+            numReviews: 10,
+            description: '2 Tepung Terigu Segitiga Biru 1 kg'
+        },
+        {
+            name: 'Kecap Manis Indofood Refill 520 ml',
+            slug: 'kecap-manis-indofood-refill-520-ml',
+            category: 'Foods & Drinks',
+            image: '/images/p5.jpg',
+            price: 14000,
+            stock: 12,
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Kecap Manis Indofood Refill 520 ml'
+        },
+        {
+            name: 'Bimoli Klasik 1 Liter Pouch',
+            slug: 'bimoli-klasik-1-liter-pouch',
+            category: 'Foods & Drinks',
+            image: '/images/p6.jpg',
+            price: 39600,
+            stock: 12,
+            rating: 4.5,
+            numReviews: 10,
+            description: '2 pcs - Bimoli Klasik 1 Liter Pouch'
+        },
+    ]
+}
+
+export default data
