5196edea450b529457cfe82573330904246d0837
Muhanaawahyu <muhanaawahyu00@gmail.com>
2023-02-25 10:03:09 +0700

    create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <header className="bg-slate-900 p-5">
+        <a href="/" className="text-slate-100 font-bold">Tukupedia</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
